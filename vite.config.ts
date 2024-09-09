b8c0bfab49a4593fe095b7ec85b10e3a5d616adf
MUmarRasheed <sfsumar@gmail.com>
2024-09-10 00:37:29 +0500

    added roleup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['/assets/HowItWorks.png']
+    }
+  }
   base: '/',  // Adjust the base URL if deploying to a subdirectory
 })
